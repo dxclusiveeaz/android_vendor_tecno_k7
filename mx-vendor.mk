4d06190b5210a5b4ad23dacf6dfd79ce6aa769d7
Pedro Pereira <pedropereirex@gmail.com>
2018-03-27 12:14:38 +0100

    New Blobs

---
+++ mx-vendor.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/lib,system/lib)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/vendor/framework,system/vendor/framework)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/uhans/mx/vendor/lib,system/vendor/lib)
